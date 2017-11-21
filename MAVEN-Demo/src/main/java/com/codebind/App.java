f95cc75923264f281a42034011801281950e3566
Raphael <Raphael@192.168.1.6>
2017-11-21 22:27:18 +0530

    Changes in print statement

--- MAVEN-Demo/src/main/java/com/codebind/App.java
+++ MAVEN-Demo/src/main/java/com/codebind/App.java
@@ -2,6 +2,6 @@ package com.codebind;
 
 public class App {
 	public static void main (String[] args) {
-		System.out.println("This is a Test Repository");
+		System.out.println("This is a Test Repository, Please handle with care !");
 	}
 }
